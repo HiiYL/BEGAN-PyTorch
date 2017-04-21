4d7b1393f3d8a6c39700c118f0e20fe8e9eea8d5
HiiYL <yonglian146@gmail.com>
2017-04-21 14:56:19 +0800

    Added arguments for setting log step and save step

--- train.py
+++ train.py
@@ -15,10 +15,7 @@ from models import G,D, weights_init
 
 from torchvision import transforms
 from os.path import join
-
-
 from tensorboard_logger import configure, log_value
-
 
 import numpy as np
 
@@ -35,14 +32,15 @@ parser.add_argument('--batchSize', type=int, default=16, help='training batch si
 parser.add_argument('--testBatchSize', type=int, default=16, help='testing batch size')
 parser.add_argument('--nEpochs', type=int, default=200, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=1e-5, help='Learning Rate. Default=0.001')
+parser.add_argument('--lr_update_step', type=float, default=100000, help='Reduce learning rate by factor of 2 every n iterations. Default=1')
 parser.add_argument('--cuda', action='store_true', help='use cuda?')
-
 
 parser.add_argument('--threads', type=int, default=8, help='number of threads for data loader to use')
 parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
 parser.add_argument('--lamb', type=int, default=100, help='weight on L1 term in objective')
 parser.add_argument('--netD', default='', help="path to netD (to continue training)")
 parser.add_argument('--netG', default='', help="path to netG (to continue training)")
+parser.add_argument('--log_step', default=10, help="logging frequency")
 
 
 parser.add_argument('--beta1', type=float, default=0.9, help='beta1 for adam. default=0.5')
@@ -67,7 +65,6 @@ if opt.cuda:
 print('===> Loading datasets')
 root_path = "dataset/"
 
-
 train_transform = transforms.Compose([
     transforms.CenterCrop(160),
     transforms.Scale(size=64),
@@ -77,10 +74,11 @@ train_transform = transforms.Compose([
 
 
 train_set = DatasetFromFolder(join(join(root_path,opt.dataset), "train"), train_transform)
-
 # test_set = get_test_set(root_path + opt.dataset)
+
 training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, batch_size=opt.batchSize, shuffle=True)
 # testing_data_loader = DataLoader(dataset=test_set, num_workers=opt.threads, batch_size=opt.testBatchSize, shuffle=False)
+
 
 print('===> Building model')
 if opt.netG:
@@ -101,11 +99,8 @@ else:
     netD = D(h=opt.h, n=opt.n, input_dim=(3,64,64))
     netD.apply(weights_init)
 
-
 print(netG)
 print(netD)
-
-
 criterion_l1 = nn.L1Loss()
 real_A = torch.FloatTensor(opt.batchSize, 3, 64, 64)
 z_D = torch.FloatTensor(opt.batchSize, opt.h)
@@ -130,8 +125,7 @@ total_iterations=0
 k_t=0
 fixed_sample = None
 fixed_x = None
-def train(epoch):
-    global total_iterations, k_t, fixed_sample, fixed_x
+def train(epoch, save_path, total_iterations, k_t, fixed_sample, fixed_x):
     for iteration, batch in enumerate(training_data_loader, 1):
         real_a_cpu = batch
         ## GT Image
@@ -151,8 +145,8 @@ def train(epoch):
         G_zG = netG(z_G)
         AE_G_zG = netD(G_zG)
 
-        d_loss_real = torch.mean(torch.abs(AE_x - real_A))#criterion_l1(AE_x, real_A)
-        d_loss_fake = torch.mean(torch.abs(AE_G_zD - G_zD))#criterion_l1(AE_G_zD, G_zD.detach())
+        d_loss_real = torch.mean(torch.abs(AE_x - real_A))#criterion_l1(AE_x, real_A) #
+        d_loss_fake = torch.mean(torch.abs(AE_G_zD - G_zD)) #criterion_l1(AE_G_zD, G_zD.detach()) ##
 
         D_loss = d_loss_real - k_t * d_loss_fake
         D_loss.backward()
@@ -161,7 +155,7 @@ def train(epoch):
         netD.zero_grad()
         netG.zero_grad()
 
-        G_loss = torch.mean(torch.abs(G_zG - AE_G_zG))#criterion_l1(G_zG, AE_G_zG.detach())
+        G_loss = torch.mean(torch.abs(G_zG - AE_G_zG)) #criterion_l1(G_zG, AE_G_zG.detach())#
         G_loss.backward()
 
         optimizerG.step()
@@ -169,9 +163,7 @@ def train(epoch):
         if fixed_sample is None:
             fixed_sample = Variable(z_G.clone().data, volatile=True)
             fixed_x = Variable(real_A.clone().data, volatile=True)
-            vutils.save_image(real_A.data, 'log/x_fixed.jpg', normalize=True,range=(-1,1))
-
-
+            vutils.save_image(real_A.data, '{}/x_fixed.jpg'.format(save_path), normalize=True,range=(-1,1))
 
         balance = ( opt.gamma * d_loss_real - G_loss ).data[0]
         measure = d_loss_real.data[0] + abs(balance)
@@ -179,57 +171,73 @@ def train(epoch):
         k_t += opt.lambda_k * balance
         k_t = max(min(1, k_t), 0)
 
-        if iteration % 10 == 0: #iteration % 1000 == 1:
-            #print(real_A.data.min())
-            #print(real_A.data.max())
+        total_iterations += 1
+
+        if total_iterations % 10 == 0:
             print("===> Epoch[{}]({}/{}): D_Loss: {:.4f} | G_Loss: {:.4f} | Measure: {:.4f} | k_t: {:.4f}".format(
                 epoch, iteration, len(training_data_loader), D_loss.data[0], G_loss.data[0], measure,k_t))
 
-        # def clip(x):
-        #     torch.max(torch.min(x, 1)
+        if total_iterations % 100 == 0:
+            log_value('D_Loss', D_loss.data[0], total_iterations)
+            log_value('G_Loss', G_loss.data[0], total_iterations)
+            log_value('Measure', measure, total_iterations)
+            log_value('k', k_t, total_iterations)
 
-
-        if iteration % 500 == 0:
-            # print(AE_G_zG.data.min())
-            # print(AE_G_zG.data.max())
-            # print(AE_x.min())
-            # print(AE_x.max())
-
+        if (total_iterations % 500 == 0) or total_iterations == 1:
             ae_x = netD(fixed_x)
             g = netG(fixed_sample)
             ae_g = netD(g)
-            total_iterations += 500
-            vutils.save_image(ae_g.data, 'log/{}_D_fake.jpg'.format(total_iterations), normalize=True,range=(-1,1))
-            vutils.save_image(ae_x.data, 'log/{}_D_real.jpg'.format(total_iterations), normalize=True,range=(-1,1))
-            vutils.save_image(g.data, 'log/{}_G.jpg'.format(total_iterations), normalize=True,range=(-1,1))
-
             
-            # log_value('Loss', loss.data[0], total_iterations)
-    # log_value('training_loss', loss.data[0], epoch)
+            vutils.save_image(ae_g.data, '{}/{}_D_fake.jpg'.format(save_path, total_iterations), normalize=True,range=(-1,1))
+            vutils.save_image(ae_x.data, '{}/{}_D_real.jpg'.format(save_path, total_iterations), normalize=True,range=(-1,1))
+            vutils.save_image(g.data, '{}/{}_G.jpg'.format(save_path, total_iterations), normalize=True,range=(-1,1))
 
+        if total_iterations % opt.lr_update_step == 0:
+            lr =  opt.lr * (0.5 ** (total_iterations//opt.lr_update_step))
+            print("reducing lr to {} at iteration {}".format(lr, total_iterations))
+            for param_group in optimizerG.param_groups:
+                param_group['lr'] = lr
+            for param_group in optimizerD.param_groups:
+                param_group['lr'] = lr
+
+    return total_iterations, k_t, fixed_sample, fixed_x
 
 def test(epoch):
     pass
 
-def checkpoint(epoch):
+def checkpoint(epoch, save_path):
     if not os.path.exists("checkpoint"):
         os.mkdir("checkpoint")
     if not os.path.exists(os.path.join("checkpoint", opt.dataset)):
         os.mkdir(os.path.join("checkpoint", opt.dataset))
-    netG_model_out_path = "checkpoint/{}/netG_model_epoch_{}_{}.pth".format(opt.dataset, epoch, datetime.datetime.now())
-    netD_model_out_path = "checkpoint/{}/netD_model_epoch_{}_{}.pth".format(opt.dataset, epoch, datetime.datetime.now())
+
+    now = datetime.datetime.now().strftime('%d%m%Y%H%M%S')
+    netG_model_out_path = "{}/netG_model_epoch_{}.pth".format(save_path,epoch)
+    netD_model_out_path = "{}/netD_model_epoch_{}.pth".format(save_path,epoch)
     torch.save(netG, netG_model_out_path)
     torch.save(netD, netD_model_out_path)
-    print("Checkpoint saved to {}".format("checkpoint" + opt.dataset))
+    print("Checkpoint saved to {}".format(save_path))
 
 
+if not os.path.exists("logs"):
+    os.mkdir("logs")
+
+if not os.path.exists(os.path.join("logs", opt.dataset)):
+    os.mkdir(os.path.join("logs", opt.dataset))
+
+now = datetime.datetime.now().strftime('%d%m%Y%H%M%S')
+save_path = os.path.join(os.path.join("logs", opt.dataset), now)
+
+if not os.path.exists(save_path):
+    os.mkdir(save_path)
+
+configure(save_path)
 for epoch in range(1, opt.nEpochs + 1):
     netG.train()
     netD.train()
-    train(epoch)
+    total_iterations, k_t, fixed_sample, fixed_x = train(epoch, save_path, total_iterations, k_t, fixed_sample, fixed_x)
     # net.eval()
     # test(epoch)
 
-
     if True: #epoch % 5 == 0:
-        checkpoint(epoch)
+        checkpoint(epoch, save_path)
